14bd139411603f00ec78051f8bdb74f2243fdf60
Mybridge Monthly <team@mybridge.co>
2018-08-26 20:03:53 +0900

    Create 08-2018.m

---
+++ src/08-2018.m
@@ -0,0 +1,78 @@
+# Machine Learning Top 10 Open Source Projects for the Past Month (v.Aug 2018)
+
+<img src="aug-ml-opensource.jpg" width="800" alt="Mybridge"></a>
+
+For the past month, we ranked nearly 250 Machine Learning Open Source Projects to pick the Top 10. 
+We compared projects with new or major release during this period. Mybridge AI ranks projects based on a variety of factors to measure its quality for professionals.
+
+* Average number of Github stars in this edition: 919⭐️
+* Topics: Auto Keras, Glow, Video to Video, Machine Translation, Dance Generator, Soccer videos to 3D, Spam Filtering, Speech Recognition, Image Generation, Face Manipulation
+
+Open source projects can be useful for programmers. Hope you find an interesting project that inspires you.
+
+<br>
+
+### Course of the month:
+
+[A) Beginners: Python for Data Science and Machine Learning Bootcamp.](http://bit.ly/2Eso3R0)[25,269 recommends, 4.5/5 stars]
+
+[B) Deep Learning: Convolutional Neural Networks in Python.](http://bit.ly/2Mbu5gc) [1,290 recommends, 4.6/5 stars]
+
+<br>
+
+## Rank 1
+### [Autokeras: An open source software library for automated machine learning (AutoML) [★2657]](https://github.com/jhfjhfj1/autokeras?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 2
+### [Glow: Code for "Glow: Generative Flow with Invertible 1x1 Convolutions" [★1671]](https://github.com/openai/glow?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 3
+### [Vid2vid: Pytorch implementation of our method for high-resolution (e.g. 2048x1024) photorealistic video-to-video translation. [★2268]](https://github.com/NVIDIA/vid2vid?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 4
+### [UnsupervisedMT: Phrase-Based & Neural Unsupervised Machine Translation - Facebook Research [★496]](https://github.com/facebookresearch/UnsupervisedMT?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 5
+### [DanceNet: Dance generator using Autoencoder, LSTM and Mixture Density Network. (Keras) [★282]](https://github.com/jsn5/dancenet?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 6
+### [Soccerontable: Upconverting YouTube soccer videos in 3D for viewing in AR/VR devices.Soccer On Your Tabletop with OpenCV [★248]](https://github.com/krematas/soccerontable?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 7
+### [Artificial-adversary: Tool to generate adversarial text examples and test machine learning models against them - Airbnb [★156]](https://github.com/airbnb/artificial-adversary?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 8
+### [Stt-benchmark: Speech to text benchmark framework [★295]](https://github.com/Picovoice/stt-benchmark?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 9
+### [Sg2im: Code for "Image Generation from Scene Graphs". A scene graph is a structured representation of a visual scene where nodes represent objects in the scene and edges represent relationships between objects [★669]](https://github.com/google/sg2im?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
+
+
+<br>
+
+## Rank 10
+### [GANimation: Anatomically-aware Facial Animation from a Single Image  [★344]](https://github.com/albertpumarola/GANimation?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more)
